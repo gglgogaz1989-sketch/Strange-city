a34afc879804cfe8e155ffef2cf599427dbcdf81
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-08 19:05:01 +0200

    Update Renderer.java

--- src/main/java/com/strangecity/engine/Renderer.java
+++ src/main/java/com/strangecity/engine/Renderer.java
@@ -1,1 +1,15 @@
+package com.strangecity.engine;
 
+import static org.lwjgl.opengl.GL11.*;
+
+public class Renderer {
+    public void init() {
+        // Настройка параметров OpenGL (глубина, туман и т.д.)
+        glEnable(GL_DEPTH_TEST); 
+    }
+
+    public void clear() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.2f, 1.0f); // Тёмно-синее небо города
+    }
+}
