4f290c06da7a276e4e1dff8fe1515fc633c01634
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-08 22:00:08 +0200

    Update MainGame.java

--- core/src/main/java/com/strangecity/MainGame.java
+++ core/src/main/java/com/strangecity/MainGame.java
@@ -3,7 +3,9 @@ package com.strangecity;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera; // Добавили камеру
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.strangecity.blocks.Platform;
 import com.strangecity.entities.Player;
@@ -11,57 +13,51 @@ import com.strangecity.utils.Button;
 
 public class MainGame extends ApplicationAdapter {
     private SpriteBatch batch;
-    private OrthographicCamera camera; // Объект камеры
+    private OrthographicCamera camera;
     private Player player;
     private Platform platform;
     private Button jumpButton;
+    private Texture cursorTex;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        
-        // Создаем камеру и настраиваем её на размер экрана
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480); // false = ось Y направлена вверх
-        
+        camera.setToOrtho(false, 800, 480);
+
         player = new Player();
         platform = new Platform();
         jumpButton = new Button("1000013987.png", 50, 50, 150);
+
+        // Создаем курсор (белый пиксель)
+        Pixmap p = new Pixmap(16, 16, Pixmap.Format.RGBA8888);
+        p.setColor(1, 1, 1, 1);
+        p.fill();
+        cursorTex = new Texture(p);
+        p.dispose();
     }
 
     @Override
     public void render() {
-        // 1. ЛОГИКА
-        player.update();
         if (jumpButton.isPressed()) player.jump();
+        player.update();
 
-        // ПРИМЕР ПОВОРОТА: Поворачиваем камеру на 1 градус каждый кадр
-        // camera.rotate(1f); 
-        
-        // Или можно установить фиксированный угол (например, 45 градусов):
-        // camera.up.set(0, 1, 0); // Сброс вектора "верх"
-        // camera.direction.set(0, 0, -1);
-        // camera.rotate(45);
-
-        camera.update(); // Обязательно обновляем матрицу камеры
-
-        // 2. ОТРИСОВКА МИРА (с учетом камеры)
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Привязываем batch к камере
+        camera.update();
         batch.setProjectionMatrix(camera.combined);
-        
         batch.begin();
         platform.draw(batch);
         player.draw(batch);
         batch.end();
 
-        // 3. ОТРИСОВКА ИНТЕРФЕЙСА (кнопка не должна крутиться вместе с миром)
-        // Сбрасываем матрицу в стандартное положение (без поворотов)
+        // Отрисовка интерфейса (кнопка и курсор)
         batch.getProjectionMatrix().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.begin();
         jumpButton.draw(batch);
+        // Рисуем курсор в позиции касания/мыши
+        batch.draw(cursorTex, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
         batch.end();
     }
 
@@ -71,5 +67,6 @@ public class MainGame extends ApplicationAdapter {
         player.dispose();
         platform.dispose();
         jumpButton.dispose();
+        cursorTex.dispose();
     }
 }
