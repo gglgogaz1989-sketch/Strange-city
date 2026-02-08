60129ef163f4c25fc2aef422d2e18fe164c47fc7
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-08 19:17:52 +0200

    Update Camera.java

--- src/main/java/com/strangecity/engine/Camera.java
+++ src/main/java/com/strangecity/engine/Camera.java
@@ -1,21 +1,8 @@
 package com.strangecity.engine;
 
 import org.joml.Vector3f;
-import org.joml.Matrix4f;
 
 public class Camera {
-    public Vector3f position;
-    public float pitch; // Наклон вверх-вниз
-    public float yaw;   // Поворот влево-вправо
-
-    public Camera(Vector3f position) {
-        this.position = position;
-    }
-
-    public Matrix4f getViewMatrix() {
-        return new Matrix4f()
-            .rotateX((float)Math.toRadians(pitch))
-            .rotateY((float)Math.toRadians(yaw))
-            .translate(-position.x, -position.y, -position.z);
-    }
+    public Vector3f pos = new Vector3f(0, 1.7f, 0); // Рост игрока
+    public float pitch, yaw; // Углы поворота
 }
