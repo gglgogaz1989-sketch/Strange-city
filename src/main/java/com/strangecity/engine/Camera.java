f08fb76be1d8e6ebd5e32377d0f1778a1921a8c9
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-08 19:21:32 +0200

    Update Camera.java

--- src/main/java/com/strangecity/engine/Camera.java
+++ src/main/java/com/strangecity/engine/Camera.java
@@ -3,6 +3,12 @@ package com.strangecity.engine;
 import org.joml.Vector3f;
 
 public class Camera {
-    public Vector3f pos = new Vector3f(0, 1.7f, 0); // Рост игрока
-    public float pitch, yaw; // Углы поворота
+    public Vector3f position;
+    public float pitch; // Вверх-вниз
+    public float yaw;   // Влево-вправо
+
+    public Camera() {
+        // Стартовая позиция игрока (чуть выше пола)
+        position = new Vector3f(0, 2.0f, 0); 
+    }
 }
